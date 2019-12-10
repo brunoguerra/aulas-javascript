8630c056c4829e5a4903ee1832ffd6f23393b7f2
Bruno Guerra <bruno@crescimento.online>
2019-12-10 19:46:41 -0300

    simple form flux

--- aula2/server/api.js
+++ aula2/server/api.js
@@ -1,14 +1,28 @@
 const express = require('express')
 var app = express()
+app.use(express.urlencoded({ extended: false }))
 
 console.log('Ola Node.JS')
 
 function home(req, res) {
-    // res.send('<b>Nosso primeiro ExpressJS</b>');
-    res.sendFile(__dirname + '/index.html')
-  }
+  // res.send('<b>Nosso primeiro ExpressJS</b>');
+  res.sendFile(__dirname + '/index.html')
+}
 
-app.get('/', home);
+app.get('/', home)
+
+app.post('/salvar', function(req, res) {
+  console.log('Entrou em salvar')
+  console.log(req.url)
+  console.log(req.body)
+
+  res.redirect('/sucesso')
+
+})
+
+app.get('/sucesso', function(req, res) {
+  res.sendFile('sucesso.html')
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
