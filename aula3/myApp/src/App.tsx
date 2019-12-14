1739b10c54c29c92c2a37bcaafd66e251add1862
Bruno Guerra <brunoguerrait@gmail.com>
2019-12-14 18:42:44 -0300

    adicionar screen

--- aula3/myApp/src/App.tsx
+++ aula3/myApp/src/App.tsx
@@ -4,6 +4,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import NovaPagina from './pages/NovaPagina';
+import Adicionar from './pages/Adicionar';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,6 +24,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import AdicionarPage from './pages/Adicionar';
 
 
 
@@ -30,6 +32,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
+        <Route path="/adicionar" component={AdicionarPage} exact={true} />
+        <Route path="/outraRota" component={NovaPagina} exact={true} />
         <Route path="/novaPagina" component={NovaPagina} exact={true} />
         <Route path="/home" component={Home} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
--- aula3/myApp/src/pages/Home.tsx
+++ aula3/myApp/src/pages/Home.tsx
@@ -1,5 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonFabList,
+} from '@ionic/react';
+
+import { add } from 'ionicons/icons'
+
+
+function meu_evento_de_click() {
+  console.log('clicou')
+  window.location.href = "/outraRota"
+}
+
+const varParaPaginaAdicionar = () => {
+  window.location.href = '/adicionar'
+}
+
+// const onClick = () => console.log('clicou')
+
+export const BotaoFlutuante: React.FC = () => (
+    <IonFab vertical="center" horizontal="end" slot="fixed">
+      <IonFabButton color="">
+        <IonIcon icon={add} onClick={varParaPaginaAdicionar} />
+      </IonFabButton>
+    </IonFab>
+);
 
 const Home: React.FC = () => {
   return (
@@ -14,7 +47,11 @@ const Home: React.FC = () => {
         <p>
           Você está na Home
         </p>
+        <a href="/outraRota" >Link para a outra rota</a>
+        <IonButton color="primary" onClick={meu_evento_de_click}>Primary</IonButton>
+        <BotaoFlutuante />
       </IonContent>
+
     </IonPage>
   );
 };
